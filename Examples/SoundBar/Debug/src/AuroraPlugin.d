39ba1729a5392c0626052454323ee064279c155a
Lei Zhang <lei@nanoleaf.me>
2017-03-14 12:05:26 -0400

    Initial commit

---
+++ Examples/SoundBar/Debug/src/AuroraPlugin.d
@@ -0,0 +1,22 @@
+src/AuroraPlugin.o: ../src/AuroraPlugin.cpp ../inc/AuroraPlugin.h \
+ ../inc/LayoutProcessingUtils.h ../inc/Point.h ../inc/Shape.h \
+ ../inc/ColorUtils.h ../inc/DataManager.h ../inc/ColorUtils.h \
+ ../inc/LayoutProcessingUtils.h ../inc/SoundUtils.h
+
+../inc/AuroraPlugin.h:
+
+../inc/LayoutProcessingUtils.h:
+
+../inc/Point.h:
+
+../inc/Shape.h:
+
+../inc/ColorUtils.h:
+
+../inc/DataManager.h:
+
+../inc/ColorUtils.h:
+
+../inc/LayoutProcessingUtils.h:
+
+../inc/SoundUtils.h:
