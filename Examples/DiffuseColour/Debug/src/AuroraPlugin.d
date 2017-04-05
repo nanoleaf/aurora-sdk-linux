b8f23db7d9d3e9b77a8b0d4d36261b7892ba4785
Lei Zhang <lei@nanoleaf.me>
2017-04-04 20:47:14 -0400

    release 1.1

---
+++ Examples/DiffuseColour/Debug/src/AuroraPlugin.d
@@ -0,0 +1,20 @@
+src/AuroraPlugin.o: ../src/AuroraPlugin.cpp ../inc/AuroraPlugin.h \
+ ../inc/LayoutProcessingUtils.h ../inc/Point.h ../inc/Shape.h \
+ ../inc/ColorUtils.h ../inc/DataManager.h ../inc/ColorUtils.h \
+ ../inc/LayoutProcessingUtils.h
+
+../inc/AuroraPlugin.h:
+
+../inc/LayoutProcessingUtils.h:
+
+../inc/Point.h:
+
+../inc/Shape.h:
+
+../inc/ColorUtils.h:
+
+../inc/DataManager.h:
+
+../inc/ColorUtils.h:
+
+../inc/LayoutProcessingUtils.h:
